79d32959a175c67be08f430eecc2308b33c82f84
commit name <dydgh1095@naver.com>
2023-07-22 19:13:53 +0900

    #1- 초기 구조
    - 모델매퍼 설정
    - DTO
    - 도메인 
    - 예외처리

---
+++ src/main/java/com/investbank/loansystem/LoanSystemApplication.java
@@ -0,0 +1,15 @@
+package com.investbank.loansystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class LoanSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/investbank/loansystem/LoanSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investbank.loansystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
