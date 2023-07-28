70c465c02e088b183fc7d74cd30ab557c5d7598b
commit name <dydgh1095@naver.com>
2023-07-28 15:41:42 +0900

    #15 - 대출 신청 도메인 설정
    - 대출 신청에 필요한 정보를 도메인으로 정의 하였다. 
    - counsel 도메인과 마찬가지로 @Where를 이용하여 isdeleted가 false인 것만 조회가 되게 하였고
    -@DynamicIsert .@DynamicUpdate를 통하여 엔티티가 생성되거나 기존엔티티가 변경되었을때 sql문이 생성되도록하였음.

---
+++ src/main/java/com/investbank/loansystem/domain/Apply.java
@@ -0,0 +1,50 @@
+package com.investbank.loansystem.domain;
+
+import lombok.*;
+import org.hibernate.annotations.*;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import java.math.*;
+import java.time.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@DynamicInsert
+@DynamicUpdate
+@Where(clause = "is_deleted=false")
+public class Apply extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long appliedId;
+
+    @Column(columnDefinition = "varchar(12) DEFAULT NULL COMMENT '신청자'")
+    private String name;
+
+    @Column(columnDefinition = "varchar(13) DEFAULT NULL COMMENT '전화번호'")
+    private String cellPhone;
+
+    @Column(columnDefinition = "varchar(50) DEFAULT NULL COMMENT '신청자 이메일'")
+    private String email;
+
+    @Column(columnDefinition = "decimal(5,4) NOT NULL COMMENT '금리'")
+    private BigDecimal interestRate;
+
+    @Column(columnDefinition = "decimal(5,4) NOT NULL COMMENT '취급수수료'")
+    private BigDecimal fee;
+
+    @Column(columnDefinition = "datetime NOT NULL COMMENT '만기'")
+    private LocalDateTime maturity;
+
+    @Column(columnDefinition = "decimal(15,2) DEFAULT NULL COMMENT '대출 신청 금액'")
+    private BigDecimal appliedAmount;
+
+    @Column(columnDefinition = "datetime DEFAULT NULL COMMENT '신청일자'")
+    private LocalDateTime appliedAt;
+}
