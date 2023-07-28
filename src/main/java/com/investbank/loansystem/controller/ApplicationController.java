75ceb15cb109faf745b64232466321d805b0d09e
commit name <dydgh1095@naver.com>
2023-07-28 17:59:14 +0900

    #19 - 대출 신청 등록 controller구현
    - 주소를 대출신청이므로 applications로 지정하고 구현
    - Postmapping으로 받아 상담 등록과 유사하기 서비스의 메소드를 이용하여 리턴

---
+++ src/main/java/com/investbank/loansystem/controller/ApplicationController.java
@@ -0,0 +1,21 @@
+package com.investbank.loansystem.controller;
+
+import com.investbank.loansystem.dto.*;
+import com.investbank.loansystem.dto.ApplicationDTO.Request;
+import com.investbank.loansystem.dto.ApplicationDTO.Response;
+import com.investbank.loansystem.service.*;
+import lombok.*;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController extends AbstractController {
+
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    public ResponseDTO<Response> create(@RequestBody Request request) {
+        return ok(applicationService.create(request));
+    }
+}
