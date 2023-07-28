2b71d08e64da432614103b1971252a73be66c2d9
commit name <dydgh1095@naver.com>
2023-07-28 13:02:14 +0900

    #11 - 대출 상담 수정 기능 controller구현
    - 수정기능이여서 PutMapping을 적용하여 update메소드를 추가하였다. 
    - 부정확한 정보여서 수정을 하지는 않았지만 iso같은 단체에서 PutMapping 같은것을 인정하지 않는다고 했다. PostMapping만으로도 충분하다고 하여  그래서 PostMapping으로 변경할까하다가 이유가 정확하게 기억나지도 않고 맥락이 제대로 기억나지 않아 수정하지는 않았다.  작은 부분이지만 추후에 변경하기로 한다.

--- src/main/java/com/investbank/loansystem/controller/CounselController.java
+++ src/main/java/com/investbank/loansystem/controller/CounselController.java
@@ -24,4 +24,10 @@ public class CounselController extends AbstractController {
     public ResponseDTO<Response> get(@PathVariable Long counselId) {
         return ok(counselService.get(counselId));
     }
+
+    @PutMapping("/{counselId}")
+    public ResponseDTO<Response> update(@PathVariable Long counselId, @RequestBody Request request) {
+        return ok(counselService.update(counselId, request));
+    } // TODO: 어느 곳에서는 특정한 이유로 PutMapping 자체를 인정하지 않고 PostMapping만 인정하기도 한다 수정할지 여부를 좀더 공부하고 개선할지 결정할것
 }
+
