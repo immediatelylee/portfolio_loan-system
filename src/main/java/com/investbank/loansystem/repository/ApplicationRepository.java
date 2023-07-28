72f6adf7f77246edbfd6b90fa362d5db33f7cebd
commit name <dydgh1095@naver.com>
2023-07-28 23:35:37 +0900

    #19 - Repository추가

---
+++ src/main/java/com/investbank/loansystem/repository/ApplicationRepository.java
@@ -0,0 +1,10 @@
+package com.investbank.loansystem.repository;
+
+import com.investbank.loansystem.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.*;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<Application, Long> {
+
+}
